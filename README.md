53b7f4cbb78740def111067780ff9bf96013861c
Sirshkoirala <seris.koerala@gmail.com>
2024-05-08 23:42:55 +0545

    feat: added data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Digital Store
+#DigitalStore
 
 
 
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -8,7 +8,7 @@ function Newsletter() {
           <h2>Newsletter</h2>
           <form>
             <input type="email" placeholder="your@email.com" />
-            <button type="submit">Subscribe</button>
+            <button className="Newsletter-btn" type="submit">Subscribe</button>
           </form>
         </div>
       </div>
