905679c8374d029a6f3c3261a08574ce7e2d05e6
Sirshkoirala <seris.koerala@gmail.com>
2024-05-08 19:38:15 +0545

    changes added

---
+++ src/components/Fernitures.jsx
@@ -0,0 +1,13 @@
+import Footer from "../Footer";
+import Newsletter from "../Newsletter";
+
+function Furnitures() {
+   return (
+      <>
+         <Newsletter />
+         <Footer />
+      </>
+   );
+}
+
+export default Furnitures;
